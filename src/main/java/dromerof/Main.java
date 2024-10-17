e1370c3f0d48b8aaa00e80a1eeb94649aefa9319
Daniel Romero <dandres508@gmail.com>
2024-10-16 19:33:39 -0500

    Conexion a la base de datos

--- src/main/java/dromerof/Main.java
+++ src/main/java/dromerof/Main.java
@@ -1,7 +1,16 @@
 package dromerof;
 
+import dromerof.db.Conexion;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Conexion conexion = new Conexion();
+        try (Connection cnx = conexion.get_connection()){
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
